71f57bd17e77e02e9b8b4d79c40b6984fa9bad39
luke-kreifels <143143799+luke-kreifels@users.noreply.github.com>
2025-03-22 15:41:39 -0500

    Update players.js - removed testing

--- players.js
+++ players.js
@@ -43,7 +43,7 @@ function renderPlayers(filteredPlayers) {
         // Create the <a> element
         const a = document.createElement('a');
         a.href = `page_not_made.html`;
-        a.textContent = `${player.firstName} ${player.lastName} testing(${player.years.join(', ')}) - Position(s): ${player.positions.join(', ')}`;
+        a.textContent = `${player.firstName} ${player.lastName} (${player.years.join(', ')}) - Position(s): ${player.positions.join(', ')}`;
 
         // Append the <a> element to the <li>
         li.appendChild(a);
